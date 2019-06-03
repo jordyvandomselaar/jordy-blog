81bd358346be593a4f421da7ae3e3e548790c6f9
Jordy van Domselaar <jordy@deinternetjongens.nl>
2019-06-03 18:53:50 +0200

    Use full width on phones

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -4,6 +4,10 @@ const Wrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr minmax(280px, ${props => props.theme.maxWidth}) 1fr;
   padding: 0 1rem;
+
+  @media (max-width: ${props => props.theme.breakpoints.phone}) {
+    padding: 0;
+  }
 `
 
 export default Wrapper
